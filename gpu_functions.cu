bb387e8b18c6c6d018bc3b1c327aa0bc019a2550
sunil <sunil@Lenovo-IdeaPad-Y510P.(none)>
2014-02-17 12:47:28 +0100

    Alter each thread needing one uchar cur_frame data

--- gpu_functions.cu
+++ gpu_functions.cu
@@ -18,7 +18,10 @@ __global__ void  motion_search(unsigned char* a,unsigned char* b, unsigned int w
 	int final_sum = 0;
 	//unsigned char Bs[256];
 
-	__shared__ uchar4 Bs[BLOCK_SIZEX * BLOCK_SIZEY];
+	//__shared__ uchar4 Bs[BLOCK_SIZEX * BLOCK_SIZEY];
+	
+	//__shared__ uchar As[BLOCK_SIZEY * (BLOCK_SIZEX + (4*2))]; // Need 2 more tiles than the blockdim.x
+
 	__shared__ int SUMs[BLOCK_SIZEX * BLOCK_SIZEY];
 	__shared__ int SUM_ROWs[BLOCK_SIZEY*BLOCK_SIZEX/4];
 	__shared__ int FINAL_SUMs[BLOCK_SIZEX/4];
@@ -26,23 +29,40 @@ __global__ void  motion_search(unsigned char* a,unsigned char* b, unsigned int w
 	__shared__ int BEST_Ys[BLOCK_SIZEX/4];
 	__shared__ int BEST_DIFFs[BLOCK_SIZEX/4];
 
-	Bs[threadIdx.y * blockDim.x + threadIdx.x] = *(((uchar4 *)b) + i * (width >> 2) + j);
+	uchar4 B;
+		
+	B = *(((uchar4 *)b) + i * (width >> 2) + j);
+	//Bs[threadIdx.y * blockDim.x + threadIdx.x] = *(((uchar4 *)b) + i * (width >> 2) + j);
+
+#if 0
+	// Load 4 tiles
+
+	if((j < (blockDim.x/4)) && (i < 16) && (j > (1920/4 - 4)) && (i > (800 - 16)))
+		As[threadIdx.y * (blockDim.x + (4*2)) + threadIdx.x] = get_pixel_word(a,j*4-15,i-15,width,height);
+	else
+		As[threadIdx.y * (blockDim.x + (4*2)) + threadIdx.x] = *(((uchar4 *)a) + (i-15) * (width >> 2) + j*4-15);
 	
 
+	// Load remaining 2 tiles
+	if(threadIdx.x < (2*4))
+		As[threadIdx.y * (blockDim.x + (4*2)) + threadIdx.x + blockDim.x] = get_pixel_word(a, j*4 + ((blockDim.x/4) - 1)*16, i-15, width, height);
 
-	if((i < 800) && (j < (1920/(blockDim.x/4))))
+	__syncthreads();
+#endif
+
+	if((i < 800) && (j < (1920/4)))
 	{
 
-		if(threadIdx.y == 0 && (threadIdx.x & 3) == 0)
+		if(threadIdx.y == 0 && threadIdx.x < (blockDim.x/4))
 		{
-			BEST_DIFFs[threadIdx.x/4] = 16*16*256;
+			BEST_DIFFs[threadIdx.x] = 16*16*256;
 		}
 		__syncthreads();
 
 		for (s=-15 ; s<16 ; s++)		// Search through a -15 to 15 neighborhood
 			for (t=-15 ; t<16 ; t++)
 			{
-				int sad=calculate_sad(a,Bs,0,0,j*4 + t,i + s,  width, height);	// Calculate difference between block from first image and second image
+				int sad=calculate_sad(a,&B,0,0,j*4 + t,i + s,  width, height);	// Calculate difference between block from first image and second image
 				SUMs[threadIdx.y * blockDim.x + threadIdx.x] = sad;
 				__syncthreads();
 
@@ -56,19 +76,19 @@ __global__ void  motion_search(unsigned char* a,unsigned char* b, unsigned int w
 				}
 
 				__syncthreads();
-				if(threadIdx.y == 0 && (threadIdx.x & 3) == 0) //sum up 16 partial sums using one thread in each tile
+				if(threadIdx.y == 0 && threadIdx.x < (blockDim.x/4)) //sum up 16 partial sums using one thread in each tile
 				{
 					for(k = 0; k < 16; k++)
 					{
-						final_sum += SUM_ROWs[blockDim.y*(threadIdx.x/(blockDim.x/4)) + k]; 
+						final_sum += SUM_ROWs[blockDim.y*threadIdx.x + k]; 
 					}
-					FINAL_SUMs[threadIdx.x/(blockDim.x/4)] = final_sum; //store it in an array of number of tiles
+					FINAL_SUMs[threadIdx.x] = final_sum; //store it in an array of number of tiles
 
-					if (FINAL_SUMs[threadIdx.x/(blockDim.x/4)] < BEST_DIFFs[threadIdx.x/(blockDim.x/4)])			// If we found a better match then store it
+					if (FINAL_SUMs[threadIdx.x] < BEST_DIFFs[threadIdx.x])			// If we found a better match then store it
 					{
-						BEST_Xs[threadIdx.x/(blockDim.x/4)] = t;
-						BEST_Ys[threadIdx.x/(blockDim.x/4)] = s;
-						BEST_DIFFs[threadIdx.x/(blockDim.x/4)] = FINAL_SUMs[threadIdx.x/(blockDim.x/4)];
+						BEST_Xs[threadIdx.x] = t;
+						BEST_Ys[threadIdx.x] = s;
+						BEST_DIFFs[threadIdx.x] = FINAL_SUMs[threadIdx.x];
 					}
 
 				}
@@ -147,7 +167,8 @@ __device__ unsigned calculate_sad(unsigned char* a, uchar4* b,  int ax, int ay, 
 	uchar4 ref_word;
 	ref_word = get_pixel_word(a,bx,by,width,height);
 
-	uchar4 from_share = b[threadIdx.y * blockDim.x + threadIdx.x];
+//	uchar4 from_share = b[threadIdx.y * blockDim.x + threadIdx.x];
+	uchar4 from_share = *b;
 	sum += ABS( from_share.x - ref_word.x );
 	sum += ABS( from_share.y - ref_word.y );
 	sum += ABS( from_share.z - ref_word.z );
